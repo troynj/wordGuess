c628b8435a2d3d696e0c688427c9d7287243c0b4
Troy Johnson <troynjohnson6@gmail.com>
2023-01-03 13:52:43 -0800

    adding outline to js

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var score = {
+    win: null,
+    loss: null
+}
+
+var timer = 10;
+
+var wordsArr = ["one", "two", "three"];
+
+function setTimer() {
+
+    var timerID = setInterval ( function() {
+        
+        timer--;
+
+        //if statement to manage gamestate
+        //clearInterval(timerID)
+
+
+    }, 1000)
+}
+
+//init game
+//display word
+//check if guess is in word
+
+//render local storage
+//win game
+//lose game
